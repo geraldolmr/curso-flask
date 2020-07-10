ff3c8a41d02626ad746ab4f27163c6c31e202e90
Geraldo Luiz <geraldolmr@gmail.com>
2020-07-10 19:10:55 -0300

    Conteudo da aula 6

--- delivery/delivery/ext/site/main.py
+++ delivery/delivery/ext/site/main.py
@@ -1,13 +1,21 @@
 from flask import request, render_template
 from flask import Blueprint
 
+from flask import current_app   # para debug (log)
+
 bp = Blueprint("site", __name__)
 
 
 @bp.route("/")
 def index():
+    current_app.logger.info("entrei na função index")
+    return render_template("index.html")
 
-    return render_template(
-        "index.html",
-        name=request.args['name']
-    )
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
+
+@bp.route("/restaurantes")
+def restaurants():
+    return render_template("restaurants.html")
